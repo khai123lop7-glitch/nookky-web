3b9a05c48b9c21daa4dd0ad36aa98716660bf62a
khai123lop7-glitch <khai123lop7@gmail.com>
2026-09-06 19:59:16 +0700

    a11y: expose homepage main target for skip link

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <main>
+      <main id="main-content" tabIndex={-1}>
         <Hero />
         <FeaturedNooks />
         <ProductGrid />
