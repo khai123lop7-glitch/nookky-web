95415f1f0ab1510577e236835989c23197e66738
votrungtin22082006-blip <votrungtin22082006@gmail.com>
2026-09-17 10:43:17 +0700

    feat(product): reuse homepage footer on product pages

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { Header } from "@/components/site/Header";
 import { ProductPurchaseSection } from "@/components/commerce/ProductPurchaseSection";
 import { ProductGallery } from "@/components/product/ProductGallery";
+import { TableOfContents } from "@/components/home/TableOfContents";
 import { formatVnd, products } from "@/data/products";
 import styles from "./ProductPage.module.css";
 
@@ -178,6 +179,8 @@ export default async function ProductPage({ params }: { params: Promise<{ slug: 
             </div>
           </div>
         </section>
+
+        <TableOfContents />
 
         <div className={styles.mobileBuy}>
           <div>
